6bd6c2066f0a4b24348d901ae695e272d21b7625
kilian.marmilliot <kilian.marmilliot@gmail.com>
2024-03-08 17:02:27 +0100

    cache with redis
    => try catch

--- src/controllers/rolesController.js
+++ src/controllers/rolesController.js
@@ -11,59 +11,79 @@ const exposeController = {
 
     },
     getRoleById: async (req, res) => {
-        const {id} = req.params
-        if (!id) {
+        try {
+            const {id} = req.params
+            if (!id) {
+                return res.sendStatus(400)
+            }
+            const role = await rolesService.findRoleById(id)
+            if (!role) {
+                return res.sendStatus(404)
+            }
+            return res.json(role)
+        } catch (error) {
             return res.sendStatus(400)
         }
-        const role = await rolesService.findRoleById(id)
-        if (!role) {
-            return res.sendStatus(404)
-        }
-        return res.json(role)
     },
     addNewRole: async (req, res) => {
-        const {body} = req
-        if (!body && !body.name) {
+        try {
+            const {body} = req
+            if (!body && !body.name) {
+                return res.sendStatus(400)
+            }
+            const newRole = await rolesService.addNewRoles(body)
+            return res.json(newRole)
+        } catch (error) {
             return res.sendStatus(400)
         }
-        const newRole = await rolesService.addNewRoles(body)
-        return res.json(newRole)
     },
     updateRole: async (req, res) => {
-        const {body} = req
-        const {id} = req.params
-        if (!id) {
+        try {
+            const {body} = req
+            const {id} = req.params
+            if (!id) {
+                return res.sendStatus(400)
+            }
+            if (await rolesService.findRoleById(id) === null) {
+                return res.sendStatus(404)
+            }
+
+            const updatedRole = await rolesService.updateRole(id, body)
+            return res.json(updatedRole)
+        } catch (error) {
             return res.sendStatus(400)
         }
-        if (await rolesService.findRoleById(id) === null) {
-            return res.sendStatus(404)
-        }
-
-        const updatedRole = await rolesService.updateRole(id, body)
-        return res.json(updatedRole)
     },
     partialUpdateRole: async (req, res) => {
-        const {body} = req
-        const {id} = req.params
-        if (!id) {
+        try {
+            const {body} = req
+            const {id} = req.params
+            if (!id) {
+                return res.sendStatus(400)
+            }
+            if (await rolesService.findRoleById(id) === null) {
+                return res.sendStatus(404)
+            }
+            const updatedRole = await rolesService.partialUpdateRole(id, body)
+            return res.json(updatedRole)
+        } catch (error) {
             return res.sendStatus(400)
         }
-        if (await rolesService.findRoleById(id) === null) {
-            return res.sendStatus(404)
-        }
-        const updatedRole = await rolesService.partialUpdateRole(id, body)
-        return res.json(updatedRole)
     },
     deleteRole: async (req, res) => {
-        const {id} = req.params
-        if (!id) {
+        try {
+            const {id} = req.params
+            if (!id) {
+                return res.sendStatus(400)
+            }
+            if (await rolesService.findRoleById(id) === null) {
+                return res.sendStatus(404)
+            }
+            const deletedRole = await rolesService.deleteRole(id)
+            return res.status(204)
+        } catch (error) {
             return res.sendStatus(400)
         }
-        if (await rolesService.findRoleById(id) === null) {
-            return res.sendStatus(404)
-        }
-        const deletedRole = await rolesService.deleteRole(id)
-        return res.status(204)
     }
 }
 
